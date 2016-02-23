0f9e4d128004d98519755f466cf1a6a258fe7429
kiselev-dv <dmitry.v.kiselev@gmail.com>
2016-02-23 23:55:44 +0500

    Fix typo

--- yekat.js
+++ yekat.js
@@ -18,15 +18,15 @@ APP = {
 angular.module('CHistory', ['rzModule', 'mePathParser'])
     .config(['$locationProvider', function($locationProvider) {
 	    $locationProvider.hashPrefix('!');
-    }]).controller('MenuController', 
-        ['$scope', '$location', 'pathParseService', 
+    }]).controller('MenuController',
+        ['$scope', '$location', 'pathParseService',
         function($scope, $location, pathParser) {
 
     $scope.menu = {};
-    
+
     pathParser.parameter("article");
     pathParser.parameter("feature");
-    
+
     $scope.$on('$locationChangeSuccess', function(){
         var p = pathParser.getParameters();
         if (p.article && APP.articles[p.article]) {
@@ -35,19 +35,19 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
         else {
             $scope.article = null;
         }
-        
+
         if(p.feature) {
-            APP.zoomToEntry(p.feature);
+            APP.zoomToEntity(p.feature);
         }
     });
-    
+
     sa = [];
     for(var i = 2020; i >= 1720; i-=10) {
         sa.push(i);
     }
     sa[0] = 2016;
     sa[sa.length - 1] = 1723;
-    
+
     $scope.slider = {
         value: 0,
         options: {
@@ -55,7 +55,7 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
             onChange: $scope.sliderUpdate
         }
     };
-    
+
     $scope.menu.layers = [{
             name: 'l1750', label: '1750', active: true, opacity: '1'
         },{
@@ -63,7 +63,7 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
             note: 'Town renamed to Sverdlovsk (Свердловск)'
         }
     ];
-    
+
     $scope.updateLayers = function() {
         for (var i = 0; i < $scope.menu.layers.length; i++) {
             if ($scope.menu.layers[i].active ) {
@@ -74,11 +74,11 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
             }
         }
     };
-    
+
     $scope.updateOpacity = function(layer) {
         APP.setAlpha(layer.name, parseFloat(layer.opacity));
     };
-    
+
     $scope.$watch('slider.value', function() {
         var v = $scope.slider.options.stepsArray[$scope.slider.value];
         if (v >= 1723 && v <= 1830) {
@@ -96,35 +96,35 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
         if (v >= 1920 && v <= 1990) {
             if('Sverdlovsk' !== APP.name) {
                 APP.setName && APP.setName('Sverdlovsk');
-                APP.name = 'Sverdlovsk';        
+                APP.name = 'Sverdlovsk';
             }
         }
         else {
             if('Yekaterinburg' !== APP.name) {
                 APP.setName && APP.setName('Yekaterinburg');
-                APP.name = 'Yekaterinburg';        
+                APP.name = 'Yekaterinburg';
             }
         }
-        
+
         APP.filterEtityByY(v);
-        
+
         $scope.updateLayers();
     });
-    
+
 }]);
 
 (function(){
-    
+
     var viewer = new Cesium.Viewer('cesiumContainer', {
         timeline:false,
         navigationInstructionsInitiallyVisible:false,
         animation: false
     });
-    
+
     setupImagery();
     setupScene();
     loadData();
-    
+
     function loadData() {
         var entityByName = {};
         var promise = Cesium.KmlDataSource.load('assets/foto.kml');
@@ -134,7 +134,7 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
             for (var i = 0; i < entities.length; i++) {
                 var entity = entities[i];
                 var ed = entity.kml.extendedData;
-                
+
                 entityByName[entity.id] = {
                     'entity': entity,
                     since: parseInt(ed.since.value),
@@ -142,7 +142,7 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
                 };
             }
         });
-        
+
         APP.filterEtityByY = function(y) {
             for (var k in entityByName) {
                 if(entityByName.hasOwnProperty(k)) {
@@ -152,13 +152,13 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
                 }
             }
         };
-        
+
         var heading = Cesium.Math.toRadians(0);
         var pitch = Cesium.Math.toRadians(-30);
         var distanceMeters = 500;
         var enityHeading = new Cesium.HeadingPitchRange(heading, pitch, distanceMeters);
-        
-        APP.zoomToEntry = function(name) {
+
+        APP.zoomToEntity = function(name) {
             if(name && entityByName[name]) {
                 viewer.zoomTo(entityByName[name].entity, enityHeading);
             }
@@ -167,27 +167,27 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
 
     function setupImagery() {
         var layers = viewer.scene.imageryLayers;
-        
+
         var s = 56.8321929;
         var n = 56.8442609;
         var w = 60.5878970;
         var e = 60.6187892;
-        
+
         var l1750 = layers.addImageryProvider(new Cesium.SingleTileImageryProvider({
             url: 'assets/1750.png',
             rectangle: Cesium.Rectangle.fromDegrees(w,s,e,n),
             credit: 'retromap.ru',
         }));
-        
+
         l1750.alpha = 1;
-        
+
         var l1924 = layers.addImageryProvider(new Cesium.TileMapServiceImageryProvider({
             url: 'assets/1924t',
             minimumLevel: 8,
             maximumLevel: 16,
             credit: 'retromap.ru'
         }));
-        
+
         l1924.alpha = 1;
 
         var layersHash = {
@@ -198,33 +198,33 @@ angular.module('CHistory', ['rzModule', 'mePathParser'])
         APP.setAlpha = function(layer, alpha) {
             if(layersHash[layer] && layersHash[layer].alpha !== undefined) {
                 layersHash[layer].alpha = alpha;
-            }            
+            }
         };
-        
+
         APP.show = function(layer) {
             if(layersHash[layer] && layers.indexOf(layersHash[layer]) < 0) {
                 layers.add(layersHash[layer]);
-            }            
+            }
         };
-        
+
         APP.hide = function(layer) {
             if(layersHash[layer] && layers.indexOf(layersHash[layer]) >= 0) {
                 layers.remove(layersHash[layer], false);
-            }            
+            }
         };
     }
 
     function setupScene() {
-        
+
         // How to place camera around point
         var heading = Cesium.Math.toRadians(0);
         var pitch = Cesium.Math.toRadians(-30);
         var distanceMeters = 10000;
-        
+
         // Zoom to
         var ekb = viewer.entities.add(ekbEntity());
         viewer.zoomTo(ekb, new Cesium.HeadingPitchRange(heading, pitch, distanceMeters));
-        
+
         APP.setName = function(name) {
             ekb.label.text = name;
         }
